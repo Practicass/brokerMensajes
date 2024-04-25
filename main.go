badd122ce44a75db647b52b6d117d1999a50eb27
Pablo Angusto <pabloangusto@gmail.com>
2024-04-25 10:31:24 +0200

    PRUEBA NO CREA COLA EXISTENTE

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"sync"
+	"time"
 )
 
-
-func main(){
+func main() {
 	broker := NuevoBroker()
 	// consumidor1 := Consumidor{nombre: "PEPE", broker: broker}
 	// consumidor2 := Consumidor{nombre: "JUAN", broker: broker}
@@ -14,31 +15,38 @@ func main(){
 	consumidor1 := NuevoConsumidor("PEPE", broker)
 	consumidor2 := NuevoConsumidor("JUAN", broker)
 	productor1 := NuevoProductor("MARIA", broker)
-	
-
 
 	consumidor1.CrearCola("COLA1")
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		consumidor1.Leer("COLA1")
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		consumidor2.Leer("COLA1")
-	}()
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		productor1.Publicar("COLA1", "HOLA")
 	}()
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		productor1.Publicar("COLA1", "ADIOS")
 	}()
+
+	time.Sleep(1000 * time.Millisecond)
+	consumidor1.CrearCola("COLA1")
+	fmt.Println("Creando cola de nuevo")
+
+	wg.Add(1)
+	go func() {
+		time.Sleep(2000 * time.Millisecond)
+		defer wg.Done()
+		consumidor1.Leer("COLA1")
+	}()
+	wg.Add(1)
+	go func() {
+		time.Sleep(2000 * time.Millisecond)
+		defer wg.Done()
+		consumidor2.Leer("COLA1")
+	}()
 	wg.Wait()
-	
+
 }
